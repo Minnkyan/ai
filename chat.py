5ca0721c922adf26f22ea1268fed78bc00cc7312
Minnkyan <a01086720910@gmail.com>
2024-05-30 16:00:07 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 st.title("ChatBot")
 
-openai.api_key = st.secrets["sk-proj-YHmgSQysIF0PgkKGetMET3BlbkFJ97fuUnc24Gk84RPYPLyO"]
-assistant = openai.ChatCompletion.create(model="gpt-4o")
+client = OpenAI(api_key=st.secrets["sk-proj-YHmgSQysIF0PgkKGetMET3BlbkFJ97fuUnc24Gk84RPYPLyO"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
