1117e79689f47abf6e5d2e6e2c0b80294187c404
Minnkyan <a01086720910@gmail.com>
2024-05-30 16:33:28 +0900

    Update openapi.py

--- openapi.py
+++ openapi.py
@@ -4,9 +4,9 @@ import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-st.title("Web App")
+st.title("웹 앱")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "질문을 입력하세요."}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -19,7 +19,7 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
