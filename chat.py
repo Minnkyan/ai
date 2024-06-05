0085d189c6ce0cf0a151da9ae56ef0c1555a34b6
Minnkyan <a01086720910@gmail.com>
2024-06-05 14:16:27 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,35 +1,3 @@
-from openai import OpenAI
-import streamlit as st
-
-def generate_image(text):
-    vector = encoder.encode_text(text)
-    image = encoder.decode(vector)
-    return image
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-
-st.title("챗봇")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "안녕하세요! 무엇을 도와드릴까요?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("OpenAI API 키를 입력하세요.")
-        st.stop()
-
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="dall-e-3", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
-
-
 def takeInput():
     # Title
     st.title('Make me an Image')
