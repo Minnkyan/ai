10224048cb15e394c938718ab8f1595c1b669aef
Minnkyan <a01086720910@gmail.com>
2024-06-12 17:36:15 +0900

    Update json.py

--- json.py
+++ json.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+from openai import OpenAI
 
 def load_data(file_path):
     try:
@@ -12,6 +13,8 @@ def load_data(file_path):
         st.error("JSON 파일을 읽는 중 오류가 발생했습니다.")
         return None
 
+
+
 data = load_data('book20.json')
 
 if data:
@@ -21,25 +24,83 @@ if data:
     pub_reviews = data.get('pubReview', [])
 
     st.title("도서 검색 웹 애플리케이션")
+    
+    key = search_title = st.text_input("key:")
+    client = OpenAI(api_key=key)
+
+    def get_similar_books(input):
+    # 파일
+        vector_store = client.beta.vector_stores.create(name="BOOK")
+
+        path = './book20_toc.json'
+        file_streams = open(path, "rb")
+
+        file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+            vector_store_id=vector_store.id,
+            files=[file_streams]
+        )
+
+        Prompt = "입력 내용과 유사한 책을 첨부 파일에서 찾아서 title과 전체 내용을 요약해서 출력해 \n출력 예: 제목:title \n -내용:...  \n\n입력: "
+
+        assistant = client.beta.assistants.create(
+            instructions= '당신은 사서입니다. 첨부 파일의 정보를 이용해 응답하세요.',
+            model="gpt-4-turbo",
+            tools=[{"type": "file_search"}],
+            tool_resources={
+                "file_search":{
+                    "vector_store_ids": [vector_store.id]
+                }
+            }
+        )
+
+        #thread
+        thread = client.beta.threads.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": Prompt + input,
+                }
+            ]
+        )
+
+        run = client.beta.threads.runs.create_and_poll( # 1초에 1회 호출 (분당 100회 제한)
+            thread_id=thread.id,
+            assistant_id=assistant.id
+        )
+
+        # message
+        thread_messages = client.beta.threads.messages.list(thread.id, run_id=run.id)
+        recommended_books = thread_messages.data[0].content[0].text.value
+        # delete thread
+        response = client.beta.threads.delete(thread.id)
+
+        # delete assistant
+        response = client.beta.assistants.delete(assistant.id)
+
+        # delete vector store
+        response = client.beta.vector_stores.delete(vector_store.id)
+        
+        return recommended_books
 
     st.header("도서 검색")
     search_title = st.text_input("도서 제목을 입력하세요:")
-
-    if search_title:
+    if st.button("검색하기"):
+        book = get_similar_books(search_title)
         matching_titles = [title for title in titles if search_title.lower() in title.lower()]
-        if matching_titles:
-            st.write(f"'{search_title}'에 대한 검색 결과:")
-            for title in matching_titles:
-                index = titles.index(title)
-                st.subheader(title)
-                st.write("**소개**")
-                st.write(introduces[index] if index < len(introduces) else "소개 정보가 없습니다.")
-                st.write("**목차**")
-                st.write(tocs[index] if index < len(tocs) else "목차 정보가 없습니다.")
-                st.write("**출판사 리뷰**")
-                st.write(pub_reviews[index] if index < len(pub_reviews) else "출판사 리뷰 정보가 없습니다.")
-        else:
-            st.write(f"'{search_title}'에 대한 검색 결과가 없습니다.")
+        st.write(book)
+        # if matching_titles:
+        #     st.write(f"'{search_title}'에 대한 검색 결과:")
+        #     for title in matching_titles:
+        #         index = titles.index(title)
+        #         st.subheader(title)
+        #         st.write("**소개**")
+        #         st.write(introduces[index] if index < len(introduces) else "소개 정보가 없습니다.")
+        #         st.write("**목차**")
+        #         st.write(tocs[index] if index < len(tocs) else "목차 정보가 없습니다.")
+        #         st.write("**출판사 리뷰**")
+        #         st.write(pub_reviews[index] if index < len(pub_reviews) else "출판사 리뷰 정보가 없습니다.")
+        # else:
+        #     st.write(f"'{search_title}'에 대한 검색 결과가 없습니다.")
 
     st.header("전체 도서 목록")
     for i, title in enumerate(titles):
