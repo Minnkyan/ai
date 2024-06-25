31dccd15ed1d82148fdcd9da3e91d2022d09ec3b
안민규 <a01086720910@gmail.com>
2024-06-26 00:22:42 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_chat import message
 
 # 메모리 초기화
 if "messages" not in st.session_state:
@@ -7,12 +6,14 @@ if "messages" not in st.session_state:
 
 # 저장한 메시지 사용자/응답 구분해서 보여주기
 for msg in st.session_state.messages:
-    message(msg["content"], is_user=msg["role"] == "user")
+    role = "user" if msg["role"] == "user" else "assistant"
+    with st.chat_message(role):
+        st.markdown(msg["content"])
 
 # 사용자 입력과 LLM 응답
 if prompt := st.chat_input("What is up?"):
     # 사용자 메시지 보여주기
-    message(prompt, is_user=True)
+    st.chat_message("user").markdown(prompt)
     
     # 메모리에 사용자 메시지 저장
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -21,11 +22,7 @@ if prompt := st.chat_input("What is up?"):
     response = f"Echo: {prompt}"
     
     # LLM 응답 보여주기
-    message(response, is_user=False)
+    st.chat_message("assistant").markdown(response)
     
     # 메모리에 LLM 응답 저장
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-
-
-
